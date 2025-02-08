ac742a63f9d288c47a8c4100e5fbcaece21eac4e
juashik <juashik@ncsu.edu>
2025-02-08 15:23:21 -0500

    Quest as a class that can be accessed globally

---
+++ android/app/src/main/java/dev/tswanson/fetchquest/android/ui/Quest.kt
@@ -0,0 +1,6 @@
+package dev.tswanson.fetchquest.android.ui
+
+data class Quest(val name: String, val description: String,
+                 val reward: String, val long: Int, val lat: Int, val dateCompleted: String) {
+
+}
