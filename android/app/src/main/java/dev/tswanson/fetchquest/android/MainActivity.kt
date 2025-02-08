0bfe86a616c5e9a907473485257fee7b956b76fc
ex4dev <31103658+ex4dev@users.noreply.github.com>
2025-02-08 12:38:44 -0500

    Add Android template

---
+++ android/app/src/main/java/dev/tswanson/fetchquest/android/MainActivity.kt
@@ -0,0 +1,62 @@
+package dev.tswanson.fetchquest.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.tswanson.fetchquest.android.ui.theme.FetchQuestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FetchQuestTheme {
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(actions = {
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /* TODO navigate to Explore */ },
+                                icon = { Icon(Icons.Filled.Home, contentDescription = "Home") }
+                            )
+                        })
+                    },
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FetchQuestTheme {
+        Greeting("Android")
+    }
+}
