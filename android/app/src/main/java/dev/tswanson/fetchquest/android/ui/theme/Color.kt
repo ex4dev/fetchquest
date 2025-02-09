b3e46e9cdc2f4095187d34a4342c8776295cf2b8
nfan2 <nfan2@ncsu.edu>
2025-02-08 23:36:27 -0500

    Changed onPrimary to white

--- android/app/src/main/java/dev/tswanson/fetchquest/android/ui/theme/Color.kt
+++ android/app/src/main/java/dev/tswanson/fetchquest/android/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val primaryColor = Color(0xFF8F592D)
 val secondaryColor = Color(0xFFCCC2DC)
 val backgroundColor = Color(0xFF7C5A3C)
-val onPrimary = Color(0xFF000000)
+val onPrimary = Color(0xFFFFFFFF)
 val surfaceVariant = Color(0xFFD2B48C)
