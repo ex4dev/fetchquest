d68dd13f6407fdd828d469ca62ba709756b760bb
nfan2 <nfan2@ncsu.edu>
2025-02-08 23:10:22 -0500

    Changed color theme

--- android/app/src/main/java/dev/tswanson/fetchquest/android/ui/theme/Color.kt
+++ android/app/src/main/java/dev/tswanson/fetchquest/android/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package dev.tswanson.fetchquest.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryColor = Color(0xFFD2B48C)
+val secondaryColor = Color(0xFFCCC2DC)
+val backgroundColor = Color(0xFF6A4E23)
+val onPrimary = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
